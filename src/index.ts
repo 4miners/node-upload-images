1fb6e4c9edbc9aeb068bf4d96fd39a724486b44a
Mariusz Serek <mariusz@serek.net>
2023-03-09 05:05:19 +0100

    Add pixhost.to image service

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,17 @@ import {
 } from './services/genericImageUploadService';
 
 import { PostimagesORG } from './services/postimagesORG';
+import { PixhostTO } from './services/pixhostTO';
 import { NewFastpicORG } from './services/newFastpicORG';
 
 export type ImageUploadServiceName =
   | 'postimages.org'
+  | 'pixhost.to'
   | 'new.fastpic.org';
 
 const services = {
   'postimages.org': PostimagesORG,
+  'pixhost.to': PixhostTO,
   'new.fastpic.org': NewFastpicORG
 };
 
